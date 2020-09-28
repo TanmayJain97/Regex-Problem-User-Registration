96c3ad6e2050ab822ca30a17abb89025a39a0d10
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-09-28 23:58:23 +0530

    Updated file UC 1

---
+++ RegexUserRegistration.java
@@ -0,0 +1,35 @@
+package com.regex_user_reg;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexUserRegistration {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String name_match = "[A-Z]{1}[a-zA-Z]{2,}";			//String pattern to be detected
+
+		String name;
+		
+		//Checking for matches
+		while(true) {
+			
+			//Entering Username
+			System.out.print("Please enter your User Name: ");
+			name = sc.nextLine();
+			
+			boolean check=name.matches(name_match);							//Condition check
+			if(check) {
+				break;
+			}else{
+				System.out.println("Incorrect format. Please try again.");
+				System.out.println();
+			}
+		}
+		System.out.println("Welcome "+name);
+		System.out.println("Program End.");
+	}
+}
